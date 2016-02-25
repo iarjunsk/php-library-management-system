9bee3d92d73d2258c2094c02c60f68eeeaabdb95
arjunsk <arjunsk15@gmail.com>
2016-02-25 16:47:05 +0530

    other commit

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	//Checks if the Database is Configured or Not
 	//Removes the error reporting
 	error_reporting(E_ERROR);
-	$conn = mysqli_connect('localhost', 'root1', '', 'library'); 
+	$conn = mysqli_connect('localhost', 'root', '', 'library'); 
 	if (!$conn) {	
 		echo "Your Database is not Configures Yet.  Click Here to ";
 		echo "<a href='setup.php'>Configure</a>";
